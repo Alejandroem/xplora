30c88a55683c442044936a718b4528ab5beed098
alejandroem <alexalejandroem@gmail.com>
2024-07-29 22:55:38 -0400

    feat: final touches on bottom dialogs

--- lib/ui/dialogs/bottom_login_card.dart
+++ lib/ui/dialogs/bottom_login_card.dart
@@ -6,103 +6,121 @@ import 'bottom_signup_card.dart';
 
 void showBottomLoginCard(BuildContext context) {
   showModalBottomSheet(
+    isScrollControlled: true,
     enableDrag: false,
     isDismissible: true,
     context: context,
     builder: (_) {
-      return Consumer(
-        builder: (context, ref, child) {
-          return Container(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                const Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 16),
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                  ),
-                  onChanged: (value) {
-                    ref
-                        .read(loginFormNotifierProvider.notifier)
-                        .setEmail(value);
-                  },
-                ),
-                const SizedBox(height: 16),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    suffixIcon: IconButton(
-                      icon: const Icon(
-                        Icons.visibility,
-                      ),
-                      onPressed: () {
-                        ref
-                            .read(loginFormNotifierProvider.notifier)
-                            .toggleObscureText();
-                      },
-                    ),
-                  ),
-                  obscureText: ref.watch(loginFormNotifierProvider).obscureText,
-                  onChanged: (value) {
-                    ref
-                        .read(loginFormNotifierProvider.notifier)
-                        .setPassword(value);
-                  },
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed:
-                      ref.watch(loginFormNotifierProvider).errors.isEmpty &&
-                              ref
-                                  .watch(loginFormNotifierProvider)
-                                  .touchedPassword &&
-                              !ref.watch(loginFormNotifierProvider).isLoading
-                          ? () async {
-                              await ref
-                                  .read(loginFormNotifierProvider.notifier)
-                                  .login();
-                              if (context.mounted) {
-                                Navigator.pop(context);
-                              }
-                            }
-                          : null,
-                  child: ref.watch(loginFormNotifierProvider).isLoading
-                      ? const SizedBox(
-                          height: 16,
-                          width: 16,
-                          child: CircularProgressIndicator(),
-                        )
-                      : const Text(
-                          'Login',
+      return Builder(builder: (context) {
+        return Padding(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: SingleChildScrollView(
+            child: Consumer(
+              builder: (context, ref, child) {
+                return Container(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
-                ),
-                const SizedBox(height: 16),
-                if (ref.watch(loginFormNotifierProvider).errors.isNotEmpty)
-                  Text(
-                    ref.watch(loginFormNotifierProvider).errors.first,
-                    style: const TextStyle(
-                      color: Colors.red,
-                    ),
+                      ),
+                      const SizedBox(height: 16),
+                      TextField(
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                        ),
+                        onChanged: (value) {
+                          ref
+                              .read(loginFormNotifierProvider.notifier)
+                              .setEmail(value);
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          suffixIcon: IconButton(
+                            icon: const Icon(
+                              Icons.visibility,
+                            ),
+                            onPressed: () {
+                              ref
+                                  .read(loginFormNotifierProvider.notifier)
+                                  .toggleObscureText();
+                            },
+                          ),
+                        ),
+                        obscureText:
+                            ref.watch(loginFormNotifierProvider).obscureText,
+                        onChanged: (value) {
+                          ref
+                              .read(loginFormNotifierProvider.notifier)
+                              .setPassword(value);
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: ref
+                                    .watch(loginFormNotifierProvider)
+                                    .errors
+                                    .isEmpty &&
+                                ref
+                                    .watch(loginFormNotifierProvider)
+                                    .touchedPassword &&
+                                !ref.watch(loginFormNotifierProvider).isLoading
+                            ? () async {
+                                await ref
+                                    .read(loginFormNotifierProvider.notifier)
+                                    .login();
+                                if (context.mounted) {
+                                  Navigator.pop(context);
+                                }
+                              }
+                            : null,
+                        child: ref.watch(loginFormNotifierProvider).isLoading
+                            ? const SizedBox(
+                                height: 16,
+                                width: 16,
+                                child: CircularProgressIndicator(),
+                              )
+                            : const Text(
+                                'Login',
+                              ),
+                      ),
+                      const SizedBox(height: 16),
+                      if (ref
+                          .watch(loginFormNotifierProvider)
+                          .errors
+                          .isNotEmpty)
+                        Text(
+                          ref.watch(loginFormNotifierProvider).errors.first,
+                          style: const TextStyle(
+                            color: Colors.red,
+                          ),
+                        ),
+                      TextButton(
+                        onPressed: () {
+                          if (context.mounted) {
+                            Navigator.pop(context);
+                          }
+                          showBottomSignUpCard(context, modal: false);
+                        },
+                        child: const Text('Don\'t have an account? Sign up'),
+                      ),
+                    ],
                   ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                    showBottomSignUpCard(context, modal: false);
-                  },
-                  child: const Text('Don\'t have an account? Sign up'),
-                ),
-              ],
+                );
+              },
             ),
-          );
-        },
-      );
+          ),
+        );
+      });
     },
   );
 }
--- lib/ui/dialogs/bottom_signup_card.dart
+++ lib/ui/dialogs/bottom_signup_card.dart
@@ -2,156 +2,170 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../infrastructure/providers/auth_providers.dart';
+import 'bottom_login_card.dart';
 
 void showBottomSignUpCard(BuildContext context, {bool modal = true}) {
   showModalBottomSheet(
     isScrollControlled: true,
-    useSafeArea: true,
     enableDrag: false,
     isDismissible: !modal,
     context: context,
     builder: (_) {
-      return Consumer(
-        builder: (context, ref, child) {
-          return SingleChildScrollView(
-            child: PopScope(
-              canPop: !modal,
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  children: [
-                    const Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      decoration: const InputDecoration(
-                        labelText: 'Username',
-                      ),
-                      onChanged: (value) {
-                        ref
-                            .read(signupFormNotifierProvider.notifier)
-                            .setUsername(value);
-                      },
-                    ),
-                    const SizedBox(height: 16),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextField(
+      return Builder(
+        builder: (context) {
+          return Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: SingleChildScrollView(
+              child: Consumer(
+                builder: (context, ref, child) {
+                  return PopScope(
+                    canPop: !modal,
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        children: [
+                          const Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          TextField(
                             decoration: const InputDecoration(
-                              labelText: 'First Name',
+                              labelText: 'Username',
                             ),
                             onChanged: (value) {
                               ref
                                   .read(signupFormNotifierProvider.notifier)
-                                  .setFirstName(value);
+                                  .setUsername(value);
                             },
                           ),
-                        ),
-                        const SizedBox(width: 16),
-                        Expanded(
-                          child: TextField(
+                          const SizedBox(height: 16),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  decoration: const InputDecoration(
+                                    labelText: 'First Name',
+                                  ),
+                                  onChanged: (value) {
+                                    ref
+                                        .read(signupFormNotifierProvider.notifier)
+                                        .setFirstName(value);
+                                  },
+                                ),
+                              ),
+                              const SizedBox(width: 16),
+                              Expanded(
+                                child: TextField(
+                                  decoration: const InputDecoration(
+                                    labelText: 'Last Name',
+                                  ),
+                                  onChanged: (value) {
+                                    ref
+                                        .read(signupFormNotifierProvider.notifier)
+                                        .setLastName(value);
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 16),
+                          TextField(
                             decoration: const InputDecoration(
-                              labelText: 'Last Name',
+                              labelText: 'Email',
                             ),
                             onChanged: (value) {
                               ref
                                   .read(signupFormNotifierProvider.notifier)
-                                  .setLastName(value);
+                                  .setEmail(value);
                             },
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      decoration: const InputDecoration(
-                        labelText: 'Email',
-                      ),
-                      onChanged: (value) {
-                        ref
-                            .read(signupFormNotifierProvider.notifier)
-                            .setEmail(value);
-                      },
-                    ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      decoration: const InputDecoration(
-                        labelText: 'Password',
-                      ),
-                      obscureText: false,
-                      onChanged: (value) {
-                        ref
-                            .read(signupFormNotifierProvider.notifier)
-                            .setPassword(value);
-                      },
-                    ),
-                    TextField(
-                      decoration: const InputDecoration(
-                        labelText: 'Confirm Password',
-                      ),
-                      obscureText: false,
-                      onChanged: (value) {
-                        ref
-                            .read(signupFormNotifierProvider.notifier)
-                            .setConfirmPassword(value);
-                      },
-                    ),
-                    const SizedBox(height: 16),
-                    if (ref.watch(signupFormNotifierProvider).errors.isNotEmpty)
-                      Text(
-                        ref.watch(signupFormNotifierProvider).errors.first,
-                        style: const TextStyle(
-                          color: Colors.red,
-                        ),
-                      ),
-                    ElevatedButton(
-                      onPressed: ref
-                                  .watch(signupFormNotifierProvider)
-                                  .errors
-                                  .isEmpty &&
+                          const SizedBox(height: 16),
+                          TextField(
+                            decoration: const InputDecoration(
+                              labelText: 'Password',
+                            ),
+                            obscureText: false,
+                            onChanged: (value) {
                               ref
-                                  .watch(signupFormNotifierProvider)
-                                  .touchedPassword &&
-                              !ref.watch(signupFormNotifierProvider).isLoading
-                          ? () async {
-                              await ref
                                   .read(signupFormNotifierProvider.notifier)
-                                  .signUp();
-
+                                  .setPassword(value);
+                            },
+                          ),
+                          TextField(
+                            decoration: const InputDecoration(
+                              labelText: 'Confirm Password',
+                            ),
+                            obscureText: false,
+                            onChanged: (value) {
+                              ref
+                                  .read(signupFormNotifierProvider.notifier)
+                                  .setConfirmPassword(value);
+                            },
+                          ),
+                          const SizedBox(height: 16),
+                          if (ref
+                              .watch(signupFormNotifierProvider)
+                              .errors
+                              .isNotEmpty)
+                            Text(
+                              ref.watch(signupFormNotifierProvider).errors.first,
+                              style: const TextStyle(
+                                color: Colors.red,
+                              ),
+                            ),
+                          ElevatedButton(
+                            onPressed: ref
+                                        .watch(signupFormNotifierProvider)
+                                        .errors
+                                        .isEmpty &&
+                                    ref
+                                        .watch(signupFormNotifierProvider)
+                                        .touchedPassword &&
+                                    !ref.watch(signupFormNotifierProvider).isLoading
+                                ? () async {
+                                    await ref
+                                        .read(signupFormNotifierProvider.notifier)
+                                        .signUp();
+          
+                                    if (context.mounted) {
+                                      Navigator.pop(context);
+                                    }
+                                  }
+                                : null,
+                            child: ref.watch(signupFormNotifierProvider).isLoading
+                                ? const SizedBox(
+                                    height: 16,
+                                    width: 16,
+                                    child: CircularProgressIndicator(),
+                                  )
+                                : const Text('Sign Up'),
+                          ),
+                          const SizedBox(height: 16),
+                          TextButton(
+                            onPressed: () {
                               if (context.mounted) {
                                 Navigator.pop(context);
                               }
-                            }
-                          : null,
-                      child: ref.watch(signupFormNotifierProvider).isLoading
-                          ? const SizedBox(
-                              height: 16,
-                              width: 16,
-                              child: CircularProgressIndicator(),
-                            )
-                          : const Text('Sign Up'),
+                              showBottomLoginCard(context);
+                            },
+                            child: const Text('Already have an account? Login'),
+                          ),
+                          const SizedBox(height: 16),
+                        ],
+                      ),
                     ),
-                    const SizedBox(height: 16),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                        showBottomSignUpCard(context, modal: false);
-                      },
-                      child: const Text('Already have an account? Login'),
-                    ),
-                    const SizedBox(height: 16),
-                  ],
-                ),
+                  );
+                },
               ),
             ),
           );
-        },
+        }
       );
     },
   );
